fdb1cf5ce5422d455b54ee2ef4d67abc26febe64
Vlad <vladok262@gmail.com>
2025-11-07 15:13:03 +0400

    Форматирование

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from micronix_ui.main_window import MainWindow
 
+
 def main():
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
